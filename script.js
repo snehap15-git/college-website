9e896c8f90cc3aa246032c6533f50286befd1291
snehap15-git <snehap15092005@gmail.com>
2025-12-17 15:17:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    alert("Thank you for your interest! Admissions opening soon.");
+}
+
+function changeColor() {
+    document.body.style.backgroundColor =
+        document.body.style.backgroundColor === "lightyellow"
+        ? "#f4f6f8"
+        : "lightyellow";
+}
